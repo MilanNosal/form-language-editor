2a48f8b3f03479372e3e99669de0b6ec468cd676
deel77@gmail.com <deel77@gmail.com@d01cc7ab-cdcf-4977-d7a0-6face547f01d>
2014-04-15 14:26:37 +0000

    transformator

---
+++ FormLanguageEditor/src/main/java/yajco/fle/generator/transform/YajcoModelToLocalModelTransformator.java
@@ -0,0 +1,14 @@
+package yajco.fle.generator.transform;
+
+import yajco.model.Language;
+
+/**
+ *
+ * @author DeeL
+ */
+public class YajcoModelToLocalModelTransformator {
+    public static yajco.fle.model.Language transform(Language language) {
+        System.out.println("Volam transformator");
+        return null;
+    }
+}
