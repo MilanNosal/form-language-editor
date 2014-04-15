e27fbfd4f68dc55aee82d617295d6d0eb7c88756
michaela.kreutzova@gmail.com <michaela.kreutzova@gmail.com@d01cc7ab-cdcf-4977-d7a0-6face547f01d>
2014-04-15 19:45:07 +0000

    tak uz idu aj ikonky, neslo to kvoli Milanovmu bugatemu kodu ---> treba dat clean & build

--- FormLanguageEditor/src/main/java/yajco/fle/panels/DirectConceptPanel.java
+++ FormLanguageEditor/src/main/java/yajco/fle/panels/DirectConceptPanel.java
@@ -17,17 +17,6 @@ public class DirectConceptPanel <T> extends javax.swing.JPanel implements PanelA
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 250, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 206, Short.MAX_VALUE)
-        );
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
