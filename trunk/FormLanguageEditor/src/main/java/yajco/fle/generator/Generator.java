6fde4e9c693de7fac96162c41456368ef7169654
deel77@gmail.com <deel77@gmail.com@d01cc7ab-cdcf-4977-d7a0-6face547f01d>
2014-04-15 09:38:20 +0000

    pridanie zavislosti na yajco-model

---
+++ trunk/FormLanguageEditor/src/main/java/yajco/fle/generator/Generator.java
@@ -0,0 +1,19 @@
+package yajco.fle.generator;
+
+import java.util.Properties;
+import javax.annotation.processing.Filer;
+import yajco.generator.FilesGenerator;
+import yajco.model.Language;
+
+/**
+ *
+ * @author DeeL
+ */
+public class Generator implements FilesGenerator{
+
+    @Override
+    public void generateFiles(Language language, Filer filer, Properties properties) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
