91d86a5db65e9091a4457bf1305b246a5f92f06e
Jaroslav.Poruban@gmail.com <Jaroslav.Poruban@gmail.com@d01cc7ab-cdcf-4977-d7a0-6face547f01d>
2014-04-17 09:38:54 +0000

    Upravy vo vzhlade a ok, cancel spravania

--- FormLanguageEditor/src/main/java/yajco/fle/panels/ConceptDialog.java
+++ FormLanguageEditor/src/main/java/yajco/fle/panels/ConceptDialog.java
@@ -1,6 +1,11 @@
 package yajco.fle.panels;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 /**
  * @author Michaela Bačíková
@@ -20,6 +25,20 @@ public class ConceptDialog extends javax.swing.JDialog {
         contentPanel.add(content);
         pack();
         setLocationRelativeTo(parent);
+        
+        //Enter/ESC behaviour        
+        rootPane.setDefaultButton(okButton);
+        
+        ActionListener cancelListener = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cancelButtonActionPerformed(e);
+            }
+        };
+        cancelButton.addActionListener(cancelListener);
+        rootPane.registerKeyboardAction(cancelListener, 
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);                
     }
 
     @SuppressWarnings("unchecked")
--- FormLanguageEditor/src/main/java/yajco/fle/panels/NewJPanel.java
+++ FormLanguageEditor/src/main/java/yajco/fle/panels/NewJPanel.java
@@ -33,10 +33,13 @@ public class NewJPanel extends javax.swing.JPanel {
         jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
 
         java.awt.GridBagLayout layout = new java.awt.GridBagLayout();
         layout.columnWidths = new int[] {0, 5, 0};
-        layout.rowHeights = new int[] {0, 5, 0};
+        layout.rowHeights = new int[] {0, 5, 0, 5, 0};
         setLayout(layout);
 
         jLabel1.setText("jLabel1");
@@ -50,9 +53,8 @@ public class NewJPanel extends javax.swing.JPanel {
         jButton1.setText("jButton1");
         jButton1.setPreferredSize(new java.awt.Dimension(73, 100));
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
         gridBagConstraints.weightx = 1.0;
         add(jButton1, gridBagConstraints);
@@ -60,17 +62,35 @@ public class NewJPanel extends javax.swing.JPanel {
         jLabel2.setText("jLabel2");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
         add(jLabel2, gridBagConstraints);
 
         jButton2.setText("jButton2");
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 2;
-        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
         add(jButton2, gridBagConstraints);
+
+        jLabel3.setText("jLabel3");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        add(jLabel3, gridBagConstraints);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 0, 0), 2));
+        jPanel1.setLayout(new java.awt.BorderLayout());
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        add(jPanel1, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.weighty = 1.0;
+        add(jPanel2, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
 
@@ -79,5 +99,8 @@ public class NewJPanel extends javax.swing.JPanel {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
