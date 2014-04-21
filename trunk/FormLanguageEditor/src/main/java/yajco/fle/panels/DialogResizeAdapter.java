3b147a5cd77e6bab7eb8ddc5b993eb8b979775ed
Jaroslav.Poruban@gmail.com <Jaroslav.Poruban@gmail.com@d01cc7ab-cdcf-4977-d7a0-6face547f01d>
2014-04-21 20:38:43 +0000

    resize pre ulet2

---
+++ trunk/FormLanguageEditor/src/main/java/yajco/fle/panels/DialogResizeAdapter.java
@@ -0,0 +1,83 @@
+package yajco.fle.panels;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.SwingUtilities;
+
+public class DialogResizeAdapter implements MouseListener, MouseMotionListener {
+
+    public enum Action {
+
+        MOVE, RESIZE_RIGHT, RESIZE_LEFT
+    }
+
+    private final JDialog dialog;
+
+    private final Action action;
+
+    private Point lastPoint, lastPosition;
+
+    private Dimension lastDimension;
+
+    public DialogResizeAdapter(JDialog dialog, JComponent actionComponent, Action action) {
+        this.dialog = dialog;
+        this.action = action;
+
+        actionComponent.addMouseListener(this);
+        actionComponent.addMouseMotionListener(this);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent evt) {
+        lastPoint = evt.getPoint();
+        lastPosition = dialog.getLocation();
+        lastDimension = dialog.getSize();
+        SwingUtilities.convertPointToScreen(lastPoint, evt.getComponent());
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent evt) {
+        Point point = evt.getPoint();
+        SwingUtilities.convertPointToScreen(point, evt.getComponent());
+        int xDelta = point.x - lastPoint.x;
+        int yDelta = point.y - lastPoint.y;
+
+        switch (action) {
+            case MOVE:
+                dialog.setLocation(lastPosition.x + xDelta, lastPosition.y + yDelta);
+                break;
+            case RESIZE_RIGHT:
+                dialog.setSize(new Dimension(lastDimension.width + xDelta, lastDimension.height + yDelta));
+                break;
+            case RESIZE_LEFT:
+                dialog.setLocation(lastPosition.x + xDelta, lastPosition.y);
+                dialog.setSize(new Dimension(lastDimension.width - xDelta, lastDimension.height + yDelta));
+                break;
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+}
