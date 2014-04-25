54b8910ad53b3952f39212bf3cfc11706f348006
milan.nosal@gmail.com <milan.nosal@gmail.com@d01cc7ab-cdcf-4977-d7a0-6face547f01d>
2014-04-25 12:11:24 +0000

    Referencie zobrazuju v zozname idcka a nie toString nad objektom

--- trunk/FormLanguageEditor/src/main/java/yajco/fle/panels/ReferencePanel.java
+++ trunk/FormLanguageEditor/src/main/java/yajco/fle/panels/ReferencePanel.java
@@ -1,26 +1,16 @@
 package yajco.fle.panels;
 
-import java.util.HashSet;
-import java.util.Observable;
-import java.util.Observer;
-import java.util.Set;
-import javax.swing.ComboBoxModel;
-import javax.swing.event.ListDataEvent;
-import javax.swing.event.ListDataListener;
-import yajco.fle.panels.references.ConceptInstanceLookup;
+import yajco.fle.panels.references.ReferenceModel;
 
 /** @author Michaela Bačíková */
 public class ReferencePanel <T> extends AbstractAccessiblePanel<T> {
-    
-    private final Class<T> concept;
 
-    // TODO: toto by malo byt dynamicke, nie? ci budem zakazdym ked sa ma zobrazit reference panel tvorit novy objekt?
-    public ReferencePanel(Class<T> concept) {
-        this.concept = concept;
+    private final ReferenceModel<T> referenceModel;
+    
+    public ReferencePanel(ReferenceModel<T> refModel) {
         initComponents();
-        ReferenceModel r = new ReferenceModel();
-        ConceptInstanceLookup.getInstance().addObserver(r);
-        valuesCombobox.setModel(r);
+        this.referenceModel = refModel;
+        valuesCombobox.setModel(referenceModel);
     }
 
     @SuppressWarnings("unchecked")
@@ -48,12 +38,12 @@ public class ReferencePanel <T> extends AbstractAccessiblePanel<T> {
        
     @Override
     public void setValue(T value) {
-        valuesCombobox.setSelectedItem(value);
+        valuesCombobox.setSelectedIndex(referenceModel.getIndexOf(value));
     }
 
     @Override
     public T getValue() {
-        return (T) valuesCombobox.getSelectedItem();
+        return this.referenceModel.getValueAt(valuesCombobox.getSelectedIndex());
     }
 
     @Override
@@ -62,52 +52,5 @@ public class ReferencePanel <T> extends AbstractAccessiblePanel<T> {
         if(valuesCombobox.isEnabled()) {
             valuesCombobox.setSelectedIndex(0);
         } 
-    }
-    
-    private class ReferenceModel implements ComboBoxModel<T>, Observer {
-        
-        private T selectedItem;
-        private final Set<ListDataListener> listeners = new HashSet<>();
-
-        public ReferenceModel() {
-        }
-
-        @Override
-        public void setSelectedItem(Object anItem) {
-            selectedItem = (T) anItem;
-        }
-
-        @Override
-        public Object getSelectedItem() {
-            return selectedItem;
-        }
-
-        @Override
-        public int getSize() {
-            return ConceptInstanceLookup.getInstance().getInstancesOfConcept(concept).size();
-        }
-
-        @Override
-        public T getElementAt(int index) {
-            return (T) ConceptInstanceLookup.getInstance().getInstancesOfConcept(concept).get(index);
-        }
-
-        @Override
-        public void addListDataListener(ListDataListener listener) {
-            listeners.add(listener);
-        }
-
-        @Override
-        public void removeListDataListener(ListDataListener listener) {
-            listeners.remove(listener);
-        }
-
-        @Override
-        public void update(Observable o, Object arg) {
-            for(ListDataListener listener : listeners) {
-                listener.contentsChanged(new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, 0, this.getSize()));
-            }
-        }
-        
     }
 }
